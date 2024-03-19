5ed9b0ba3a913ada3fc13f0eb1ccbdcdab870ad1
gabryHM <gabrielmessa3@gmail.com>
2024-03-19 16:27:55 +0100

    feat: done

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+var multer = require('multer');
 
 var app = express();
+var upload = multer({ dest: 'uploads/' })
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
+});
+
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res) {
+  var file = req.file;
+  res.json({
+    name: file.originalname,
+    type: file.mimetype,
+    size: file.size
+  });
 });
 
 
